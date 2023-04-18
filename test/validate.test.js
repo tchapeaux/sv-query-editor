12cad8aef7e2ed2e1238e1b0862a20885937fb45
Thomas Chapeaux <tc@saper-vedere.eu>
2023-04-18 15:32:06 +0200

    ref: improve DX of debugging tests

--- test/validate.test.js
+++ test/validate.test.js
@@ -3,10 +3,10 @@ import { describe, expect, it } from "vitest";
 import validate from "../src/utils/svQueryValidate";
 
 function checkHasMarker(query, markerRow, markerCol) {
-  const markers = validate(query);
-  return markers.some(
-    (m) => m.startColumn === markerCol && m.startLineNumber === markerRow
-  );
+  const marker = getFirstMarker(query);
+
+  expect(marker.startColumn).toBe(markerCol);
+  expect(marker.startLineNumber).toBe(markerRow);
 }
 
 function checkIsValidated(query) {
@@ -14,30 +14,40 @@ function checkIsValidated(query) {
   return markers.length === 0;
 }
 
+function getFirstMarker(query) {
+  const markers = validate(query);
+  expect(markers).toHaveLength(1);
+  return markers[0];
+}
+
 describe("Validate query", () => {
-  it("Check open parenthesis", () => {
-    expect(checkHasMarker("(", 1, 1)).toBeTruthy();
-    expect(checkHasMarker("(foo) OR (bar", 1, 10)).toBeTruthy();
+  it("Detect wrong open parenthesis", () => {
+    checkHasMarker("(", 1, 1);
+    checkHasMarker("(foo) OR (bar", 1, 10);
   });
 
-  it("Check closed parenthesis", () => {
-    expect(checkHasMarker(")", 1, 1)).toBeTruthy();
-    expect(checkHasMarker("(foo) OR bar)", 1, 13)).toBeTruthy();
+  it("Detect wrong closed parenthesis", () => {
+    checkHasMarker(")", 1, 1);
+    checkHasMarker("(foo) OR bar)", 1, 13);
   });
 
-  it("Check OR typos", () => {
-    expect(checkHasMarker(" or ", 1, 2)).toBeTruthy();
-    expect(checkHasMarker(" Or ", 1, 2)).toBeTruthy();
-    expect(checkHasMarker(" oR ", 1, 2)).toBeTruthy();
-    expect(checkHasMarker(" PR ", 1, 2)).toBeTruthy();
-    expect(checkHasMarker(" pr ", 1, 2)).toBeTruthy();
-    expect(checkHasMarker(" IR ", 1, 2)).toBeTruthy();
-    expect(checkHasMarker(" ir ", 1, 2)).toBeTruthy();
-    expect(checkHasMarker(" ro ", 1, 2)).toBeTruthy();
-    expect(checkHasMarker(" RO ", 1, 2)).toBeTruthy();
+  it("Detect OR typos", () => {
+    checkHasMarker(" or ", 1, 2);
+    checkHasMarker(" Or ", 1, 2);
+    checkHasMarker(" oR ", 1, 2);
+    checkHasMarker(" PR ", 1, 2);
+    checkHasMarker(" pr ", 1, 2);
+    checkHasMarker(" IR ", 1, 2);
+    checkHasMarker(" ir ", 1, 2);
+    checkHasMarker(" ro ", 1, 2);
+    checkHasMarker(" RO ", 1, 2);
+
+    checkHasMarker("aa OR bb OR cc RO dd", 1, 16);
   });
 
-  it("Valid queries", () => {
-    expect(checkIsValidated("(foo) OR (bar)")).toBeTruthy();
+  it("Check valid queries are validated", () => {
+    checkIsValidated("(foo) OR (bar)");
+    checkIsValidated("aa OR bb -(cc OR dd) stakeholder:National");
+    checkIsValidated('"foo bar baz" @foobar #barfoo');
   });
 });
